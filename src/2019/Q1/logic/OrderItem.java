3556f1af7e36aee89e5ee2826876e1a2f7f0a791
Kananate Watewitee <kananate24@gmail.com>
2024-01-20 15:50:10 +0700

    OrderItem Complete

--- src/2019/Q1/logic/OrderItem.java
+++ src/2019/Q1/logic/OrderItem.java
@@ -1,5 +1,34 @@
 package logic;
 
-public class OrderItem {
+import java.security.PublicKey;
 
+public class OrderItem {
+    private Item item;
+    private int itemAmount;
+
+    public OrderItem(Item item, int itemAmount){
+        setItem(item);
+        setItemAmount(itemAmount);
+    }
+    public void increaseItemAmount(int amount){
+        setItemAmount(getItemAmount() + Math.max(0,amount));
+    }
+    public int calculateTotalPrice(){
+        return getItemAmount() * getItem().getPricePerPiece();
+    }
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getItemAmount() {
+        return itemAmount;
+    }
+
+    public void setItemAmount(int itemAmount) {
+        this.itemAmount = Math.max(0 , itemAmount);
+    }
 }
